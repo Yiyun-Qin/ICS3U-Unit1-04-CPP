6efe909ea577b4f845a584fe2255be8898346dce
Yiyun Qin <yiyun.qin@mths.ca>
2022-02-24 19:54:40 +0000

    math program

---
+++ math.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 Yiyun Qin All rights reserved
+//
+// Created by Yiyun Qin
+// Created at February 2022
+// This is a "Hello World" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a triangle has the dimensions:" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is " << (5 * 3) << " cm²." << std::endl;
+    std::cout << "Perimetre is " << 2 * (5 + 3) << " cm." << std::endl;
+}
